fc1a36246fbd39f95a7288d51ab8a17fd27e69cb
ripples <564311873@qq.com>
2016-01-11 13:12:47 +0800

    Bug fix.

--- src/com/android/reverse/hook/XposeHookHelperImpl.java
+++ src/com/android/reverse/hook/XposeHookHelperImpl.java
@@ -1,16 +1,26 @@
 package com.android.reverse.hook;
 
 import java.lang.reflect.Member;
+import java.util.Arrays;
 
-
+import com.android.reverse.util.Logger;
 import de.robv.android.xposed.XposedBridge;
 
 public class XposeHookHelperImpl implements HookHelperInterface {
 
-	@Override
-	public void hookMethod(Member method, MethodHookCallBack callback) {
-		// TODO Auto-generated method stub
-		XposedBridge.hookMethod(method, callback);
-	}
+    @Override
+    public void hookMethod(Member method, MethodHookCallBack callback) {
+        // TODO Auto-generated method stub
+        if (method != null) {
+            XposedBridge.hookMethod(method, callback);
+        } else {
+            Logger.log("method try to hook not found");
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                Logger.log(Arrays.toString(e.getStackTrace()));
+            }
+        }
+    }
 
 }
